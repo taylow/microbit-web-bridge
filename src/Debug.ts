030e0117aee1fe874a664d9a5133a113d6205f38
Yuriy <yuriy.voznyak@globallogic.com>
2019-06-12 14:07:02 +0300

    add logger library; unified format (#17)

--- src/Debug.ts
+++ src/Debug.ts
@@ -1,34 +1,9 @@
 import $ = require('jquery');
-import {DEBUG, TIMESTAMPS} from "./constants/Config";
 
 
-export enum DebugType {
-    ERROR = "ERROR",
-    DEBUG = "DEBUG",
-    WARNING = "WARNING"
-}
-
-export function debug(message: any, type?: DebugType) {
+export function terminalMsg(message: any) {
     const terminal = $('#terminal-contents');
-
-    let consoleString = "";
-    let terminalString = "";
-
-    if(TIMESTAMPS) {
-        consoleString += `[${new Date().toISOString().slice(11,-5)}] `;
-        terminalString = consoleString;
-    }
-
-    if(type != null) {
-        consoleString += `${type}: `;
-    }
-
-    if(DEBUG) {
-        consoleString += message;
-        terminalString += message;
-        console.log(consoleString);
-    }
-
-    terminal.text(`${terminal.text()}${terminalString}\n`);
+    const terminalMsg = `[${new Date().toISOString().slice(11,-5)}] ${message}`;
+    terminal.text(`${terminal.text()}${terminalMsg}\n`);
     terminal.scrollTop(terminal[0].scrollHeight)
 }
